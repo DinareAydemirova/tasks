4122156f0a47a79b66fa2019d501f17809be291d
DinareAydemirova <dinaraaydamirova@gmail.com>
2024-04-24 19:42:09 +0400

    react

---
+++ react/react/src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../main.css'
+const Main = () => {
+  return (
+    <div className='main'>
+        <input type="text" placeholder='search'/>
+        <button>search</button>
+    </div>
+  )
+}
+
+export default Main
